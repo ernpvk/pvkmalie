90361eece0b806d9e30679afd7ca94fee5dc1e5b
earaen <123929693+earaen@users.noreply.github.com.>
2024-12-29 04:32:55 +0700

    fix: add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      plugins: [visualizer()],
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          home: ['./src/pages/Home'],
-          about: ['./src/pages/AboutDetail'],
+          vendor: ["react", "react-dom", "react-router-dom"],
+          home: ["./src/pages/Home"],
+          about: ["./src/pages/AboutDetail"],
         },
       },
     },
